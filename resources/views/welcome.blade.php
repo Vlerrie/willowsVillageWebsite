9834c13cb36c7df53b47556f8652052520a8497d
Vlerrie <roodt.brendan@gmail.com>
2022-11-10 12:49:17 +0200

    Added vendor files because I cant do composer update on shared hosting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,8 +12,15 @@
     @auth()
         <div class="container-fluid col-12 px-4 py-3 bg-light mb-5 shadow">
             <div class="row align-items-center g-lg-5 py-5 ps-lg-5">
-                    <div class="col-lg-6">
-                        <h1 class="display-4 fw-bold lh-1 mb-3">Welcome Back {{ Auth::user()->name }}</h1>
+                    <div class="col-lg-12">
+{{--                        <h1 class="display-4 fw-bold lh-1 mb-3">Welcome Back {{ Auth::user()->name }}</h1>--}}
+                        @if(count($news) == 0)
+                            <h1 class="display-4 px-5 fw-bold lh-1 mb-3">No news or updates at this time</h1>
+                        @else
+                            <h1 class="display-4 px-5 fw-bold lh-1 mb-3">Latest Updates:</h1>
+                            @include('partials.newsItems')
+                        @endif
+
                     </div>
             </div>
         </div>
@@ -47,11 +54,7 @@
             </div>
             <div class="col-md-10 mx-auto col-lg-6 pe-lg-1 align-items-top">
                 @auth()
-                    @if(count($news) == 0)
-                        <h1 class="display-4 px-5 fw-bold lh-1 mb-3">No news or updates at this time</h1>
-                    @endif
 
-                    @include('partials.newsItems')
 
                 @else
                     @include('auth.registerForm')
