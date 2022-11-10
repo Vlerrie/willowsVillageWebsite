37c0bda6f1792962e596908ecd2e45e793fd7adc
Vlerrie <roodt.brendan@gmail.com>
2022-11-10 16:58:35 +0200

    fix jquery

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,26 +30,22 @@
         <div class="row align-items-center g-lg-5 py-5 ps-lg-5">
             <div class="col-lg-6 text-center text-lg-start">
                 <h1 class="display-4 fw-bold lh-1 mb-3">Willows Security Village</h1>
-                <p class="col-lg-11 fs-4">Joining is free and gives every resident a voice.<br><br>
-                    By joining you will be added to a mailing list and will receive general updates that affects the
-                    area and its people.
-                    You will be able to unsubscribe from this feature should you not want to be included on these
-                    emails.<br><br>
-                    With the help and guidance of a consultant with conciderable experience in the field of
-                    community
-                    enclosures we as the willows security village task team are beyond excited to announce that it
-                    IS
-                    possible to start with closing down our community.
-
-                    We will need an absolute unified front and total support of the community to drive this project
-                    to completion. *NEED MORE INFO & DETAIL*
-
-                    {{-- AGM details will also be posted here and sent via email.
-                    In the future we also plan to use this platform to allow for an easy way of giving
-                    recommendations and voting on important matters. --}}
+                <p class="col-lg-11 fs-4">
+                    Joining is free and gives every resident a voice.
                     <br><br>
-                    We hope to build a unified and strong community that looks out for our suburb and its peoples,
-                    help us by joining and being an active member!
+                    By joining, you will be added to a mailing list and will receive general updates, that affect the
+                    area and its people. You will be able to unsubscribe from this feature, should you prefer not to be
+                    included on these emails.
+                    <br><br>
+                    With the help and guidance of a consultant, with considerable experience in the field of community
+                    enclosures, we as the Willows Security Village task team, are beyond excited, to announce that it IS
+                    possible and extremely important, to start with the process to close down our area to safeguard our
+                    residents. We will, however, need an absolute unified front and total support of the community, to
+                    drive this project to completion. NEED MORE INFO & DETAIL
+                    <br><br>
+                    We strive towards a unified and strong community, that will look out to improve the safety and
+                    well-being of our people and our suburb. So we urge each and everyone to help us, by joining this
+                    site and being an active member of our community
                 </p>
             </div>
             <div class="col-md-10 mx-auto col-lg-6 pe-lg-1 align-items-top">
@@ -68,17 +64,17 @@
         <div class="row align-items-center g-lg-5 py-5 ps-lg-5">
             <div class="col-lg-7 text-center text-lg-start">
                 <h1 class="display-4 fw-bold lh-1 mb-3">Die Wilgers</h1>
-                <p class="col-lg-10 fs-4"> Die Wilgers is one of the eastern suburb of Pretoria, Gauteng.
-                    It can be described as a safe residential area, rich in trees and with beautiful gardens and
-                    sidewalks.
-                    The presence of an Afrikaans and English high school in the area, and two primary schools
-                    nearby, as
-                    well as the German school in close proximity, makes Die Wilgers a sought after suburb.
-                    The residents of Die Wilgers strive to maintain the special character of the area through the
-                    actions of a Residents Association represented by an executive committee which is democratically
-                    elected at its public annual general meeting.
-                    The association represents residents in the area enclosed by Rubida Street to the West, Simon
-                    Vermooten Road to the East, the N4 to the North and Bronberg as the Southern border.</p>
+                <p class="col-lg-10 fs-4">
+                    Die Wilgers is one of the eastern suburbs of Pretoria East in Gauteng. It can be described as a
+                    peaceful and well kept residential area, rich in trees and with beautiful gardens and sidewalks. The
+                    presence of various shops, Die Wilgers Hospital, the Afrikaans and English High schools, two primary
+                    schools, as well as the German school, in close proximity, makes Die Wilgers a very popular and
+                    sought after suburb. The residents of Die Wilgers strive to maintain the special character of the
+                    area, through the actions of a Residents Association, represented by an executive committee, which
+                    is democratically elected at its public annual general meeting. The association represents residents
+                    in the area enclosed by Rubida Street to the West, Simon Vermooten Road to the East, the N4 to the
+                    North and Bronberg as the Southern border.
+                </p>
             </div>
             <div class="col-md-10 mx-auto col-lg-5 pe-lg-5" id="wilgersMap">
                 <iframe
