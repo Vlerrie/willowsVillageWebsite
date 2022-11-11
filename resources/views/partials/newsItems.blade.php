eaa995566581bc67334dd4701bf11c00e3624d50
Vlerrie <roodt.brendan@gmail.com>
2022-11-11 11:34:43 +0200

    interface enhancements

--- resources/views/partials/newsItems.blade.php
+++ resources/views/partials/newsItems.blade.php
@@ -2,14 +2,14 @@
     @foreach($news as $n)
         <li class="list-group-item border-start-0 border-end-0 p-4">
             <div class="row">
-                <div class="col-md-3 border-end">
+                <div class="col-md-4">
                     <h2 class="h2 mb-0">
                         {{$n->subject}}
                     </h2>
                     <small>{{$n->getPublished()}}</small>
                     <hr class="d-md-none">
                 </div>
-                <div class="col-md-9 ps-3">
+                <div class="col-md-8 ps-3">
                     @auth()
                         @if(Auth::user()->admin)
                             <button class="btn btn-outline-danger border-0 float-end"
