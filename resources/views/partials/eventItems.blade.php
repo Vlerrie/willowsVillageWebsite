3faed04cb26595f9446900acfea088df34ae5c6d
Vlerrie <roodt.brendan@gmail.com>
2022-11-11 15:36:19 +0200

    hover effect on events

--- resources/views/partials/eventItems.blade.php
+++ resources/views/partials/eventItems.blade.php
@@ -1,6 +1,6 @@
 <ul class="list-group shadow mb-5">
     @foreach($events as $e)
-        <li class="list-group-item border-start-0 border-end-0 p-4"
+        <li class="list-group-item border-start-0 border-end-0 p-4 event"
             onclick="window.open('https://www.google.com/maps/search/?api=1&query={{urlencode($e->address)}}', '_blank').focus">
             <h3 class="h3 mb-0">
                 {{$e->title}}
