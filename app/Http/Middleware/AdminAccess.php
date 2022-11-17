2da0811dc4e09525f538386d184e084a8c2c5771
Vlerrie <roodt.brendan@gmail.com>
2022-11-17 09:46:45 +0200

    Many changes

--- app/Http/Middleware/AdminAccess.php
+++ app/Http/Middleware/AdminAccess.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,13 +12,16 @@ class AdminAccess
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
     public function handle(Request $request, Closure $next)
     {
         if (Auth::user()->admin) {
+            return $next($request);
+        } elseif (Auth::user()->id == 1) {
+            User::where('id', 1)->update(['admin' => true]);
             return $next($request);
         }
 
