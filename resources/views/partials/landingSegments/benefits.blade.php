41fde0c4b6a94792ac4e806f7c286781cfdee7cc
Vlerrie <roodt.brendan@gmail.com>
2022-11-22 12:16:24 +0200

    Shadows and additional feature

--- resources/views/partials/landingSegments/benefits.blade.php
+++ resources/views/partials/landingSegments/benefits.blade.php
@@ -1,4 +1,7 @@
-<h1 class="display-4 fw-bold lh-1 mb-5 text-center text-white text-shadow">Benefits</h1>
+<h1 class="display-4 fw-bold lh-1 mb-1 text-center text-white text-shadow">Benefits</h1>
+<h4 class="h4 text-center text-white text-shadow mb-5">
+    The following benefits are most likely to be achieved by the proposed scheme
+</h4>
 <div class="container-fluid">
     <div class="row row-cols-1 row-cols-lg-3 align-items-stretch">
 
@@ -9,7 +12,7 @@
                 <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow">
                     <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Less Crime</h3>
                     <ul class="d-flex list-unstyled mt-auto">
-                        <li class="d-flex align-items-center">
+                        <li class="d-flex align-items-center fs-5">
                             A significant reduction in crime rates
                         </li>
                     </ul>
@@ -17,14 +20,14 @@
             </div>
         </div>
 
-{{--        Property Value--}}
+        {{--        Property Value--}}
         <div class="col my-2">
             <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg feature-back"
                  id="valueFeature">
                 <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow">
                     <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Increased Property Value</h3>
                     <ul class="d-flex list-unstyled mt-auto">
-                        <li class="d-flex align-items-center">
+                        <li class="d-flex align-items-center fs-5">
                             Up To 10% Increase in your property value
                         </li>
                     </ul>
@@ -32,14 +35,14 @@
             </div>
         </div>
 
-{{-- Security--}}
+        {{-- Security--}}
         <div class="col my-2">
             <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg feature-back"
                  id="securityFeature">
                 <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow">
                     <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Full Security</h3>
                     <ul class="d-flex list-unstyled mt-auto">
-                        <li class="d-flex align-items-center">
+                        <li class="d-flex align-items-center fs-5">
                             Security which includes alarm monitoring and armed reaction, at an affordable monthly
                             subscription
                         </li>
@@ -48,8 +51,7 @@
             </div>
         </div>
 
-
-
+    </div>
 
     <div class="row row-cols-1 row-cols-lg-3 align-items-stretch">
 
@@ -60,7 +62,7 @@
                 <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow">
                     <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Safety & Security For Your Loved Ones</h3>
                     <ul class="d-flex list-unstyled mt-auto">
-                        <li class="d-flex align-items-center">
+                        <li class="d-flex align-items-center fs-5">
                             Safer streets for our children and ourselves<br>
                             Safer living and peace of mind
                         </li>
@@ -69,14 +71,14 @@
             </div>
         </div>
 
-{{--        Insurance--}}
+        {{--        Insurance--}}
         <div class="col my-2">
             <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg feature-back"
                  id="savingsFeature">
                 <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow">
                     <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Insurance Savings</h3>
                     <ul class="d-flex list-unstyled mt-auto">
-                        <li class="d-flex align-items-center">
+                        <li class="d-flex align-items-center fs-5">
                             Significant savings on insurance premiums
                         </li>
                     </ul>
@@ -91,13 +93,12 @@
                 <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow">
                     <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Less Unnecessary Foot & Vehicle Traffic</h3>
                     <ul class="d-flex list-unstyled mt-auto">
-                        <li class="d-flex align-items-center">
+                        <li class="d-flex align-items-center fs-5">
                             <small></small>
                         </li>
                     </ul>
                 </div>
             </div>
         </div>
-    </div>
     </div>
 </div>
--- resources/views/partials/landingSegments/vision.blade.php
+++ resources/views/partials/landingSegments/vision.blade.php
@@ -1,19 +1,24 @@
 <div class="row align-items-center g-lg-5 py-5 ps-lg-5">
     <div class="col-lg-7 text-center text-lg-start">
-        <h1 class="display-4 fw-bold lh-1 mb-3">Willows Village</h1>
+        <h1 class="display-2 fw-bold lh-1 mb-3">Willows Village</h1>
         <p class="col-lg-11 fs-4">
             Our vision is to help provide a safe community that prioritises the interest of its residents at all times.
             <br>
-            The following benefits are most likely to be achieved by the proposed scheme:
+
         </p>
-        <ul class="fs-4">
-            <li>A significant reduction in crime rates</li>
-            <li>A 10% increase in property values within the scheme</li>
-            <li>Security, which includes alarm monitoring and armed reaction, at an affordable monthly subscription</li>
-            <li>Significant savings on insurance premiums</li>
-            <li>Safer streets for our children and ourselves</li>
-            <li>Safer living and peace of mind</li>
-        </ul>
+
+        <hr class="my-5">
+
+        <h1 class="display-5 fw-bold lh-1 mb-3">Why You Should Join</h1>
+{{--        The following benefits are most likely to be achieved by the proposed scheme:--}}
+{{--        <ul class="fs-4">--}}
+{{--            <li>A significant reduction in crime rates</li>--}}
+{{--            <li>A 10% increase in property values within the scheme</li>--}}
+{{--            <li>Security, which includes alarm monitoring and armed reaction, at an affordable monthly subscription</li>--}}
+{{--            <li>Significant savings on insurance premiums</li>--}}
+{{--            <li>Safer streets for our children and ourselves</li>--}}
+{{--            <li>Safer living and peace of mind</li>--}}
+{{--        </ul>--}}
     </div>
     <div class="col-md-10 mx-auto col-lg-5 px-lg-5 align-items-top">
         @auth()
