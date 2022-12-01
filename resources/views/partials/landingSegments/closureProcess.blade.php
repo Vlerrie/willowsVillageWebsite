4da8f7ec9d6b0f602b41c4ac6c071d31bc1e0607
Vlerrie <roodt.brendan@gmail.com>
2022-12-01 07:59:02 +0200

    Phases included.

--- resources/views/partials/landingSegments/closureProcess.blade.php
+++ resources/views/partials/landingSegments/closureProcess.blade.php
@@ -1,34 +1,61 @@
 <div class="row align-items-center g-lg-5 py-5">
 
-    <div class="col-lg-7 col-md-6 mx-auto ps-lg-5" id="wilgersMap">
+    <div class="col-lg-6 col-md-6 mx-auto ps-lg-5" id="wilgersMap">
         <h1 class="display-4 fw-bold lh-1 mb-3">The Closure Process</h1>
         <h3 class="display-5 ">Phase 1</h3>
         <p class="col-lg-10 fs-4">
-            <i class="fa fa-check-square text-success"></i> All residents of the potential affected area needs to be made
-            aware of the intention for closure in the form of a public presentation.
+            <i class="fa fa-square text-secondary"></i> Decide on the model and costs
         </p>
         <p class="col-lg-10 fs-4">
-            <i class="fa fa-square text-secondary"></i> A commitment of more than 80% of the affected area's
-            residents needs to be reached in the form of a digital form with intention to pay as soon as 80% is reached.
+            <i class="fa fa-square text-secondary"></i> Contract Managing Agent and Streetsafe
         </p>
         <p class="col-lg-10 fs-4">
-            <i class="fa fa-square text-secondary"></i> A traffic impact study needs to be done.
+            <i class="fa fa-square text-secondary"></i> Divide area into streets with street leaders
         </p>
         <p class="col-lg-10 fs-4">
-            <i class="fa fa-square text-secondary"></i> Official application submitted for approval.
+            <i class="fa fa-square text-secondary"></i> Distribute the welcome pack documentation
+        </p>
+        <p class="col-lg-10 fs-4">
+            <i class="fa fa-square text-secondary"></i> Public announcement and presentation meeting
+        </p>
+        <p class="col-lg-10 fs-4">
+            <i class="fa fa-square text-secondary"></i> Gather 80% support with payment commitment
+        </p>
+        <p class="col-lg-10 fs-4">
+            <i class="fa fa-square text-secondary"></i> Inform the community when 80% is reached and start first debit order
         </p>
 
         <hr class="col-4 my-5">
 
         <h3 class="display-5 ">Phase 2</h3>
         <p class="col-lg-10 fs-4">
-            <i class="fa fa-square text-secondary"></i> Gate Installation
+            <i class="fa fa-square text-secondary"></i> Traffic Impact Study (TIS)
         </p>
         <p class="col-lg-10 fs-4">
-            <i class="fa fa-square text-secondary"></i> Some more stuff
+            <i class="fa fa-square text-secondary"></i> Prepare application with supporting documentation and resident commitments
+        </p>
+        <p class="col-lg-10 fs-4">
+            <i class="fa fa-square text-secondary"></i> Submit Application
+        </p>
+
+        <hr class="col-4 my-5">
+
+        <h3 class="display-5 ">Phase 3</h3>
+        <p class="col-lg-10 fs-4">
+            <i class="fa fa-square text-secondary"></i> Receive permission by council
+        </p>
+
+        <hr class="col-4 my-5">
+
+        <h3 class="display-5 ">Phase 4</h3>
+        <p class="col-lg-10 fs-4">
+            <i class="fa fa-square text-secondary"></i> Construction of gates and guard houses start
+        </p>
+        <p class="col-lg-10 fs-4">
+            <i class="fa fa-square text-secondary"></i> Camera and Boom implementation
         </p>
     </div>
-    <div class="col-lg-5 col-md-6 pe-lg-5">
+    <div class="col-lg-6 col-md-6 pe-lg-5 align-self-start">
         <h1 class="display-4 fw-bold lh-1 my-3">Closure Map</h1>
         <iframe title="Willows Village" src="https://www.google.com/maps/d/u/0/embed?mid=1Wrxh4XcS_ZXLJd6-n4Ctl3J3eJbMsFc&ehbc=2E312F"
                 width="100%" height="600px"></iframe>
